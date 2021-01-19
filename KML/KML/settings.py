8e911c8d471f81afdf731e152cb0ba6fe45ddf7c
hrishi2016 <gurukala2016@gmail.com>
2021-01-19 15:04:45 +0530

    Change for heroku

--- KML/KML/settings.py
+++ KML/KML/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '+*j(yi(@u^lc&_z&02q2=38#u@&(7q)m@%7b@92@9$g_tb8#8-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- KML/KML/urls.py
+++ KML/KML/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from django.urls import path , include
-
+from django.urls import path, include
+from django.conf import settings
+from django.views.static import serve
+from django.conf.urls import url
 urlpatterns = [
-    path('',include('library.urls')),
+    path('', include('library.urls')),
     path('admin/', admin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve,
+        {'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_ROOT}),
 ]
